16a7508dbf64cd4d932459d37af4ddf751d67dfb
dhawit <tdhawit@gmail.com>
2024-07-15 04:39:06 +0545

    app_data

---
+++ client_side/lib/utility/app_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart' show Color;
+
+class AppColor {
+  const AppColor._();
+
+  static const darkOrange = Color(0xFF455A64);
+  static const lightOrange = Color(0xFFFFEB3B);
+
+  static const darkGrey = Color(0xFFA6A3A0);
+  static const lightGrey = Color(0xFFE5E6E8);
+}
